1da53501ee459fe7a67f8422cab1a98d00fc4aac
13851731474 <13851731474@localhost>
2020-09-01 12:04:07 +0800

    fix bug when route url contains search or query

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,19 @@ export function isTabActive(tabKey: string, location: any) {
 }
 
 export function isLocationChanged(prevLoca: any, currLoca: any) {
-  const { key, ...otherPrevloca } = prevLoca;
-  const { key: currKey, ...otherCurrloca } = currLoca;
+  const { key, search, ...otherPrevloca } = prevLoca;
+  const { key: currKey, search: currSearch, ...otherCurrloca } = currLoca;
+
+  if (otherPrevloca.query) {
+    for (const key in otherPrevloca.query) {
+      otherPrevloca.query[key] = otherPrevloca.query[key].toString();
+    }
+  }
+  if (otherCurrloca.query) {
+    for (const key in otherCurrloca.query) {
+      otherCurrloca.query[key] = otherCurrloca.query[key].toString();
+    }
+  }
+
   return !isEqual(otherPrevloca, otherCurrloca);
 }
